171fa913694c335221f96b490f1afc644274c1c9
Sean Aguinaga <sean@preconceptiontest.com>
2019-10-25 18:54:16 -0700

    Fixed broken thing

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
-import React, {
-  unstable_ConcurrentMode as ConcurrentMode,
-  StrictMode
-} from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "todomvc-app-css/index.css";
 // import './index.css';
-import 'todomvc-common/base.css';
-import 'todomvc-app-css/index.css';
-import App from './App';
-import AppHeader from './components/AppHeader';
+import "todomvc-common/base.css";
+import App from "./App";
+import AppHeader from "./components/AppHeader";
 
 ReactDOM.render(
-  <ConcurrentMode>
-    <StrictMode>
-      <AppHeader />
-      <App />
-    </StrictMode>
-  </ConcurrentMode>,
-  document.getElementById('root')
+  <>
+    <AppHeader />
+    <App />
+  </>,
+  document.getElementById("root")
 );
